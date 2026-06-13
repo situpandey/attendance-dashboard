2935aad47de1fef08836ca804763612d768ec22e
situpandey <situpandey79@gmail.com>
2026-06-14 00:03:00 +0530

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+function updateDateTime() {
+
+const now = new Date();
+
+const dateOptions = {
+weekday: 'long',
+year: 'numeric',
+month: 'long',
+day: 'numeric'
+};
+
+document.getElementById("date").innerHTML =
+now.toLocaleDateString("en-US", dateOptions);
+
+document.getElementById("time").innerHTML =
+now.toLocaleTimeString();
+
+}
+
+updateDateTime();
+
+setInterval(updateDateTime, 1000);
+
+
 function login(){
 
 let empid =
@@ -6,32 +30,27 @@ document.getElementById("empid").value;
 let password =
 document.getElementById("password").value;
 
+if(empid==="TM-001" && password==="12345"){
 
-if(
-empid==="TM-001"
-&&
-password==="12345"
-){
-
-window.location.href=
-"dashboard.html";
+window.location.href="dashboard.html";
 
 }
 
 else{
 
-alert(
-"Invalid Employee ID or Password"
-);
+alert("Invalid Employee ID or Password");
 
 }
 
 }
+
+
 function checkIn(){
 
 alert("✅ Attendance Marked Successfully");
 
 }
+
 
 function checkOut(){
 
