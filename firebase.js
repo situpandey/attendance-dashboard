2645d23658b5a7b45bb3846918565acd26d4bb80
situpandey <situpandey79@gmail.com>
2026-06-15 14:23:37 +0530

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// Firebase Config
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "attendence-app-94a2e.firebaseapp.com",
+  projectId: "attendence-app-94a2e",
+  storageBucket: "attendence-app-94a2e.firebasestorage.app",
+  messagingSenderId: "66194698417",
+  appId: "YOUR_APP_ID"
+};
