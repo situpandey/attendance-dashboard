35059c5c13a2c2ac754cb39b554a198861c36fef
situpandey <situpandey79@gmail.com>
2026-06-15 17:25:37 +0530

    Refactor Firebase configuration and initialization

--- firebase.js
+++ firebase.js
@@ -1,10 +1,19 @@
-// Firebase Config
-
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "attendence-app-94a2e.firebaseapp.com",
-  projectId: "attendence-app-94a2e",
-  storageBucket: "attendence-app-94a2e.firebasestorage.app",
-  messagingSenderId: "66194698417",
-  appId: "YOUR_APP_ID"
+
+apiKey: "YOUR_API_KEY",
+
+authDomain: "attendence-app-94a2e.firebaseapp.com",
+
+projectId: "attendence-app-94a2e",
+
+storageBucket: "attendence-app-94a2e.firebasestorage.app",
+
+messagingSenderId: "66194698417",
+
+appId: "YOUR_APP_ID"
+
 };
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
